f2a8327471901b8801db9cc5a8f56ee45ae0be30
Suraj <surajgautam1111@gmai;l.com>
2023-06-20 17:54:59 +0545

    working on filter

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import { CartState } from "../context/Context"
 import SingleProduct from "./SingleProduct"
-
+import './style.css'
 const Home = () => {
   const {state :{products}}=CartState()
   console.log(products)
   return (
     <div className="home">
     {/* <Filter/> */}
-    <div className="prodcutContainer">
+    <div className="productContainer">
       {products.map((prod,index)=>{
         return <SingleProduct prod={prod} key ={prod.id}/>
       })}
